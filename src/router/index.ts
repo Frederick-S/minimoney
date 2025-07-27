4ada9ac775bf8245ff538dc2c847b181b4e39a65
Xiaodan Mao <mao_xiaodan@hotmail.com>
2025-07-27 14:20:12 +0800

    Switch router to use hash history mode
    Replaces createWebHistory with createWebHashHistory in the Vue router configuration. This change may improve compatibility with static file servers that do not support history mode routing.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useSupabase } from '../composables/useSupabase'
 import HomeView from '../components/HomeView.vue'
 import ChartsView from '../components/ChartsView.vue'
@@ -31,7 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
